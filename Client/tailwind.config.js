21947e47d468b8043b3a9cda08d68ba8b7a97e60
58777037+mfragger@users.noreply.github.com <58777037+mfragger@users.noreply.github.com>
2023-10-17 23:24:30 +0800

    first commit

---
+++ Client/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{razor,html,cshtml}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        source:['source-serif-pro', 'serif'],
+        libre:['libre-franklin', 'sans-serif']
+      },
+      fontSize: {
+        'pp-5xl': '46px',
+        'pp-4xl': '32px',
+        'pp-3xl': '28px',
+        'pp-2xl': '26px',
+        'pp-xl': '24px',
+        'pp-l': '18px',
+        'pp-base': '16px',
+        'pp-sm':'12px',
+      },
+      screens: {
+          'xsm' : '533px',
+          '2xsm' : '444px'
+      },
+      colors: {
+        'pasta-palete-yellow': '#FAF3D4',
+        'pasta-palete-black': '#231F20',
+        'pasta-palete-orange': '#E3B746',
+        'pasta-palete-yellow-2': '#EDB72D',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+],
+}
+
